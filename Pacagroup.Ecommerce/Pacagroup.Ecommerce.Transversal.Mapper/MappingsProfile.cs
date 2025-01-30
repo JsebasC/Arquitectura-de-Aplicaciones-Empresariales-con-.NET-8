1c3c36b0c88e4cc40d435ac75acf58e4a99a97af
sebastian.cuellar <sebastian.cuellar@ceiba.com.co>
2025-01-30 10:04:59 -0500

    Capa aplicacion

---
+++ Pacagroup.Ecommerce/Pacagroup.Ecommerce.Transversal.Mapper/MappingsProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Pacagroup.Ecommerce.Application.DTO;
+using Pacagroup.Ecommerce.Domain.Entity;
+
+namespace Pacagroup.Ecommerce.Transversal.Mapper
+{
+    public class MappingsProfile : Profile
+    {
+        public MappingsProfile()
+        {
+            CreateMap<Customers, CustomersDto>().ReverseMap();
+            //CreateMap<Customers, CustomersDto>().ReverseMap()
+            //    .ForMember(destination => destination.CustomerId, source => source.MapFrom(src => src.CustomerId));
+        }
+    }
+}

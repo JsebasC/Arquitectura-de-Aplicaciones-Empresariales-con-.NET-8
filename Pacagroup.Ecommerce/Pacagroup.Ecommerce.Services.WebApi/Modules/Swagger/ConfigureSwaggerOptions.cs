fcfdf745d908dee53f390e2f7c4b6884d2b07875
sebastian.cuellar <sebastian.cuellar@ceiba.com.co>
2025-02-24 18:46:54 -0500

    Control de versiones utilizando parametros Query String

---
+++ Pacagroup.Ecommerce/Pacagroup.Ecommerce.Services.WebApi/Modules/Swagger/ConfigureSwaggerOptions.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Pacagroup.Ecommerce.Services.WebApi.Modules.Swagger
+{
+    public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+    {
+        readonly IApiVersionDescriptionProvider apiVersionDescriptionProvider;
+
+        public ConfigureSwaggerOptions(IApiVersionDescriptionProvider apiVersionDescriptionProvider)
+        {
+            this.apiVersionDescriptionProvider = apiVersionDescriptionProvider;
+        }
+
+        static OpenApiInfo CreateInfoForApiVersion(ApiVersionDescription description)
+        {
+            var info = new OpenApiInfo
+            {
+                Version = description.ApiVersion.ToString(),
+                Title = "Pacagroup Technology Services API Market",
+                Description = "A simple example ASP.NET Core Web API",
+                TermsOfService = new Uri("https://pacagroup.com/terms"),
+                Contact = new OpenApiContact
+                {
+                    Name = "Alex Espejo",
+                    Email = "alex.espejo.c@gmail.com",
+                    Url = new Uri("https://pacagroup.com/contact")
+                },
+                License = new OpenApiLicense
+                {
+                    Name = "Use under LICX",
+                    Url = new Uri("https://pacagroup.com/licence")
+                }
+            };
+
+            return info;
+        }
+
+        public void Configure(SwaggerGenOptions options)
+        {
+            foreach (var description in apiVersionDescriptionProvider.ApiVersionDescriptions)
+            {
+                options.SwaggerDoc(description.GroupName, CreateInfoForApiVersion(description));
+            }
+        }
+    }
+}

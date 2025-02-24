5ddf557685ed7155d946553cc8f4e1a676435421
sebastian.cuellar <sebastian.cuellar@ceiba.com.co>
2025-02-24 18:04:18 -0500

    Actualizacion swagger extension

--- Pacagroup.Ecommerce/Pacagroup.Ecommerce.Services.WebApi/Modules/Swagger/SwaggerExtensiones.cs
+++ Pacagroup.Ecommerce/Pacagroup.Ecommerce.Services.WebApi/Modules/Swagger/SwaggerExtensiones.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.OpenApi.Models;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 namespace Pacagroup.Ecommerce.Services.WebApi.Modules.Swagger
 {
@@ -6,45 +8,55 @@ namespace Pacagroup.Ecommerce.Services.WebApi.Modules.Swagger
     {
         public static IServiceCollection AddSwagger(this IServiceCollection services)
         {
+            // Register the Swagger generator, defining 1 or more Swagger documents
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                c.SwaggerDoc("v1", new OpenApiInfo
                 {
-                    Title = "Mi API",
                     Version = "v1",
+                    Title = "Pacagroup Technology Services API Market",
                     Description = "A simple example ASP.NET Core Web API",
-                    Contact = new Microsoft.OpenApi.Models.OpenApiContact
+                    TermsOfService = new Uri("https://pacagroup.com/terms"),
+                    Contact = new OpenApiContact
                     {
-                        Name = "Johan Cuellar",
-                        Email = ""
+                        Name = "Alex Espejo",
+                        Email = "alex.espejo.c@gmail.com",
+                        Url = new Uri("https://pacagroup.com/contact")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under LICX",
+                        Url = new Uri("https://pacagroup.com/licence")
                     }
                 });
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
 
-                c.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+                var securityScheme = new OpenApiSecurityScheme
                 {
-                    Description = "Ingrese un token válido",
                     Name = "Authorization",
+                    Description = "Enter JWT Bearer token **_only_**",
                     In = ParameterLocation.Header,
-                    Type = SecuritySchemeType.ApiKey
-                });
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    BearerFormat = "JWT",
+                    Reference = new OpenApiReference
+                    {
+                        Id = JwtBearerDefaults.AuthenticationScheme,
+                        Type = ReferenceType.SecurityScheme
+                    }
+                };
+
+
+                c.AddSecurityDefinition(securityScheme.Reference.Id, securityScheme);
 
                 c.AddSecurityRequirement(new OpenApiSecurityRequirement
                 {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer",
-                            }
-                        },
-                        new string[] { }
-                    }
+                    { securityScheme, new List<string>() { } }
                 });
-
             });
-
             return services;
         }
     }

2ee819cf6861920c1e427d78f3f9c1f1cd57b20f
sebastian.cuellar <sebastian.cuellar@ceiba.com.co>
2025-03-01 15:23:35 -0500

    Implementacion de la Capa de Servicios

--- Pacagroup.Ecommerce/Pacagroup.Ecommerce.Services.WebApi/Modules/Feature/FeatureExtensions.cs
+++ Pacagroup.Ecommerce/Pacagroup.Ecommerce.Services.WebApi/Modules/Feature/FeatureExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace Pacagroup.Ecommerce.Services.WebApi.Modules.Feature
+﻿using System.Text.Json.Serialization;
+
+namespace Pacagroup.Ecommerce.Services.WebApi.Modules.Feature
 {
     public static class FeatureExtensions
     {
@@ -14,6 +16,12 @@
                     .WithHeaders("authorization", "accept", "content-type", "origin")
                     .WithMethods("GET", "PUT", "POST", "DELETE", "OPTIONS");
                 });
+            });
+
+            services.AddControllers().AddJsonOptions(opts =>
+            {
+                var enumConverter = new JsonStringEnumConverter();
+                opts.JsonSerializerOptions.Converters.Add(enumConverter);
             });
 
             return services;
--- Pacagroup.Ecommerce/Pacagroup.Ecommerce.Services.WebApi/Program.cs
+++ Pacagroup.Ecommerce/Pacagroup.Ecommerce.Services.WebApi/Program.cs
@@ -15,8 +15,7 @@ using WatchDog;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 

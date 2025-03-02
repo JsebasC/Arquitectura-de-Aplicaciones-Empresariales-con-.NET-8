e12fe5d88a87927b3911bb3c96aa16dc8dc9ea1a
sebastian.cuellar <sebastian.cuellar@ceiba.com.co>
2025-03-01 19:10:23 -0500

    Se añade Redoc

--- Pacagroup.Ecommerce/Pacagroup.Ecommerce.Services.WebApi/Controllers/v2/CategoriesController.cs
+++ Pacagroup.Ecommerce/Pacagroup.Ecommerce.Services.WebApi/Controllers/v2/CategoriesController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.RateLimiting;
+using Pacagroup.Ecommerce.Application.DTO;
 using Pacagroup.Ecommerce.Application.Interface.Features;
+using Pacagroup.Ecommerce.Transversal.Common;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace Pacagroup.Ecommerce.Services.WebApi.Controllers.v2
 {
@@ -10,6 +13,7 @@ namespace Pacagroup.Ecommerce.Services.WebApi.Controllers.v2
     [ApiController]
     [Route("api/v{version:apiVersion}/[controller]")]
     [ApiVersion("2.0")]
+    [SwaggerTag("Obtener Categorias de Productos")]
     public class CategoriesController : ControllerBase
     {
         private readonly ICategoriesApplication _categoriesApplication;
@@ -19,6 +23,12 @@ namespace Pacagroup.Ecommerce.Services.WebApi.Controllers.v2
         }
 
         [HttpGet("GetAll")]
+        [SwaggerOperation(Summary = "Get Categorias",
+            Description = "This endpoint will return all categories",
+            OperationId = "GetAll",
+            Tags = new string[] {"GetAll"})]
+        [SwaggerResponse(200, "List Of Categories", typeof(Response<IEnumerable<CategoryDto>>))]
+        [SwaggerResponse(404, "Not Found Categories", typeof(Response<IEnumerable<CategoryDto>>))]
         public async Task<IActionResult> GetAllAsync()
         {
             var response = await _categoriesApplication.GetAll();
